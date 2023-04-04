b25fd94e3761568ae2dcd3e5ce0b1cca9f3b1f63
Christopher Swinton <chris@theswintons.co.uk>
2023-04-04 21:33:59 +0100

    Added gatsby-plugin-netlify

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,19 +4,31 @@
 module.exports = {
   siteMetadata: {
     title: `Christopher Swinton`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://www.chrisswinton.co.uk/`,
   },
-  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-image",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+    `gatsby-plugin-netlify`,
+    // this (optional) plugin enables Progressive Web App + Offline functionality
+    // To learn more, visit: https://gatsby.dev/offline
+    // `gatsby-plugin-offline`,
+  ],
 };
