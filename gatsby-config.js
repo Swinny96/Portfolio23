fd1beb3044f4b0db5acaf4fe2d17a100ca207440
Christopher Swinton <chris@theswintons.co.uk>
2023-04-04 21:17:28 +0100

    Fixed build

--- gatsby-config.js
+++ gatsby-config.js
@@ -4,30 +4,19 @@
 module.exports = {
   siteMetadata: {
     title: `Christopher Swinton`,
-    description: `Christopher Swintons Portfolio Website`,
-    author: `@cswinton`,
-    siteUrl: `https://www.chrisswinton.co.uk/`,
+    siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [
-    "gatsby-plugin-sass",
-    "gatsby-plugin-google-gtag",
-    "gatsby-plugin-image",
-    "gatsby-plugin-sitemap",
-    {
-      resolve: "gatsby-plugin-manifest",
-      options: {
-        icon: "src/images/favicon.png",
-      },
+  plugins: ["gatsby-plugin-styled-components", "gatsby-plugin-image", "gatsby-plugin-sitemap", {
+    resolve: 'gatsby-plugin-manifest',
+    options: {
+      "icon": "src/images/icon.png"
+    }
+  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
+    resolve: 'gatsby-source-filesystem',
+    options: {
+      "name": "images",
+      "path": "./src/images/"
     },
-    "gatsby-plugin-sharp",
-    "gatsby-transformer-sharp",
-    {
-      resolve: "gatsby-source-filesystem",
-      options: {
-        name: "images",
-        path: "./src/images/",
-      },
-      __key: "images",
-    },
-  ],
+    __key: "images"
+  }]
 };
